35471277984594f1afff51c6758cf2d4483cad0a
Emi Vilche <31260215+Doomlash@users.noreply.github.com>
2022-03-19 13:38:38 -0300

    resolucion ejercicios 1.4 y 1.5

--- Challenge Algoritmos/4.Random_number.py
+++ Challenge Algoritmos/4.Random_number.py
@@ -1,1 +1,19 @@
+from random import randint, seed
+from datetime import datetime
+
 if __name__ == "__main__":
+    #lo seedeo con el tiempo para que siempre sea distinto
+    seed(datetime.now())
+    n = randint(0, 1000)
+    guess = -1
+    print("Intenta adivinar un número entre 0 y 1000.")
+    #hago los chequeos y el bucle continua hasta que el numero se adivina
+    while guess != n:
+        guess = int(input("-->"))
+        if guess < n:
+            print("El número es mayor al dado")
+        if guess > n:
+            print("el número es menor al dado")
+        if guess == n:
+            print("Felicitaciones, adivinaste el numero!!")
+
